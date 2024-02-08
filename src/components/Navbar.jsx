5457a03569e93363db71683149439292177d77d8
Sauhardya <116086552+Sauhardya27@users.noreply.github.com>
2024-02-08 23:41:08 +0530

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -6,7 +6,7 @@ const Navbar = () => {
 	const [toggle, setToggle] = useState(false);
 	return (
 		<nav className='w-full flex py-6 justify-between items-center navbar'>
-			<img src={logo} alt="hazexpert" className='w-[124px] h-[32px] z-[2]' />
+			<img src={logo} alt="hazexpert" className='w-[124px] h-[32px] z-[5]' />
 
 			<ul className='list-none sm:flex hidden justify-end items-center flex-1'>
 				{navLinks.map((nav, index) => (
